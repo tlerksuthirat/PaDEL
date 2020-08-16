a547ed0da729e9cd7aa968673f5fb14cc599aaeb
tlerksuthirat <tassanee.lerk@gmail.com>
2020-08-16 17:33:07 +0700

    Update PaDEL.sh

--- PaDEL.sh
+++ PaDEL.sh
@@ -1,5 +1,5 @@
 java -Xms1G -Xmx1G -Djava.awt.headless=true -jar 
-./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -detectAromaticity -fingerprints -descriptortypes 
+./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -detectAromaticity -fingerprints -descriptortypes
 ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir 
 ./PaDEL-Descriptor/KlekotaRothFingerprintCount.xml -dir 
 ./PaDEL-Descriptor/Atom -dir 
