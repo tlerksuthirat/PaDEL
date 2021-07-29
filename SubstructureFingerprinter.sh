f960ab4adcf741cdad0612ce2071526412316b86
tlerksuthirat <tassanee.lerk@gmail.com>
2021-07-29 15:39:06 +0700

    Update SubstructureFingerprinter.sh

--- SubstructureFingerprinter.sh
+++ SubstructureFingerprinter.sh
@@ -1,4 +1,1 @@
-java -Xms1G -Xmx1G -Djava.awt.headless=true -jar 
-./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -retainorder -log -standardizetautomers -standardizenitro -detectaromaticity -fingerprints -descriptortypes 
-./PaDEL-Descriptor/SubstructureFingerprinter.xml -dir 
-./ -file descriptors_output_SubstructureFingerprinter.csv
+java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -retainorder -log -standardizetautomers -standardizenitro -detectaromaticity -fingerprints -descriptortypes ./PaDEL-Descriptor/SubstructureFingerprinter.xml -dir ./ -file descriptors_output_SubstructureFingerprinter.csv
