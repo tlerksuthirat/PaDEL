90bfd15c0929c3dc63540505743c6bdacc276183
tlerksuthirat <tassanee.lerk@gmail.com>
2021-07-29 12:58:21 +0700

    Update PubchemFingerprinter.sh

--- PubchemFingerprinter.sh
+++ PubchemFingerprinter.sh
@@ -1,1 +1,4 @@
-java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -threads=-1 -retainorder -log -standardizetautomers -standardizenitro -detectaromaticity -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output_PubchemFingerprinter.csv
+java -Xms1G -Xmx1G -Djava.awt.headless=true -jar 
+./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -retainorder -log -standardizetautomers -standardizenitro -detectaromaticity -fingerprints -descriptortypes 
+./PaDEL-Descriptor/PubchemFingerprinter.xml -dir 
+./ -file descriptors_output_PubchemFingerprinter.csv
